ca9ce2a5d4d08694b260a84d990cfc845cd755fd
Nathan <100944663+nathzzi@users.noreply.github.com>
2025-03-31 00:45:47 +0800

    Update ui-test.lua

--- ui-test.lua
+++ ui-test.lua
@@ -1,1 +1,4 @@
+setreadonly(client, false);
+client.enableautoexec()
+setreadonly(client, true);
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Spongebob-ass/Spongebob/refs/heads/main/main.lua"))()
