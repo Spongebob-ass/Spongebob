16dbdaefa706ad0b60592c19aa61b9fd81210029
Nathan <100944663+nathzzi@users.noreply.github.com>
2025-07-20 19:50:52 +0800

    Update environment_extensions.lua

--- devstuff/environment_extensions.lua
+++ devstuff/environment_extensions.lua
@@ -19,14 +19,20 @@ elseif _cheatname:find("Hybrid") then
 end
 
 -- // function
---[[
-_getgenv.firesignal = newcclosure(function(signal, ...)
-    assert(typeof(signal) == "RBXScriptSignal", string.format("invalid argument #1 to 'firesignal' (RBXScriptSignal expected, got %s)", typeof(signal)))
-    for _, v in ipairs(getconnections(signal)) do
-        task.spawn(v.Function, ...)
+_getgenv.getscriptclosure = newcclosure(function(_targetScript)
+    for _, _regEntry in pairs(_getreg()) do
+        if type(_regEntry) == "table" then
+            for _, _funcEntry in pairs(_regEntry) do
+                if type(_funcEntry) == "function" then
+                    local _env = getfenv(_funcEntry)
+                    if _env and rawget(_env, "script") == _targetScript then
+                        return _funcEntry
+                    end
+                end
+            end
+        end
     end
 end)
-]]--
 
 _getgenv.getscripthash = newcclosure(function(instance)
     assert(typeof(instance) == "Instance", "invalid argument #1 to 'getscripthash' (Instance expected, got " .. typeof(instance) .. ")", 2)
@@ -36,3 +42,4 @@ end)
 
 -- // aliases
 _getgenv.dumpstring = _getgenv.getscripthash
+_getgenv.getscriptfunction = _getgenv.getscriptclosure
