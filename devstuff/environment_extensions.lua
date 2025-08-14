4e1f3509ba06445ce7bdf0b4384128a6c0194f5f
Nathan <100944663+nathzzi@users.noreply.github.com>
2025-08-15 00:09:04 +0800

    Added getscripts, sethiddenproperty, gethiddenproperty

--- devstuff/environment_extensions.lua
+++ devstuff/environment_extensions.lua
@@ -12,14 +12,16 @@ local _loadstring = clonefunction(loadstring);
 local _require = clonefunction(_getrenv.require);
 local _pcall = clonefunction(_getrenv.pcall);
 
+local _players = cloneref(game:GetService("Players"))
+
 if _cheatname:find("Frostware Gen X") then
-    -- // I will add sum codes here incase something bad happened
+    -- # I will add sum codes here incase something bad happened
 elseif _cheatname:find("Hybrid") then
-    -- // Hybrid is so cool
+    -- # Hybrid is so cool
 end
 
--- // function
--- // FUCK I BROKE THIS IN C++
+-- # function
+-- # FUCK I BROKE THIS IN C++
 _getgenv.getloadedmodules = newcclosure(function()
     local t = {}
     for _, v in pairs(getinstances()) do
@@ -51,6 +53,45 @@ _getgenv.getscripthash = newcclosure(function(instance)
     return instance:GetHash()
 end)
 
--- // aliases
+-- # will this work?.. ( will convert to c++ later )
+-- # functions below are from ballistic
+-- # credits to ballistic <3
+_getgenv.gethiddenproperty = newcclosure(function(inst, idx)
+    assert(typeof(inst) == "Instance", "invalid argument #1 to 'gethiddenproperty' [Instance expected]", 2)
+    local was = isscriptable(inst, idx)
+    setscriptable(inst, idx, true)
+
+    local val = inst[idx]
+    setscriptable(inst, idx, was)
+
+    return val, not was
+end)
+
+_getgenv.sethiddenproperty = newcclosure(function(inst, idx, val)
+    assert(typeof(inst) == "Instance", "invalid argument #1 to 'sethiddenproperty' [Instance expected]", 2)
+    local was = isscriptable(inst, idx)
+    setscriptable(inst, idx, true)
+
+    inst[idx] = val
+
+    setscriptable(inst, idx, was)
+
+    return not was
+end)
+
+_getgenv.getrunningscripts = newcclosure(function()
+    local t = {}
+    for _, v in ipairs(_players.LocalPlayer:GetDescendants()) do
+        if v:IsA("LocalScript") or v:IsA("ModuleScript") then
+            table.insert(t, v)
+        end
+    end
+    return t
+end)
+
+-- # aliases
+_getgenv.getscripts = _getgenv.getrunningscripts
+_getgenv.set_hidden_property = _getgenv.sethiddenproperty
+_getgenv.get_hidden_property = _getgenv.gethiddenproperty
 _getgenv.dumpstring = _getgenv.getscripthash
 _getgenv.getscriptfunction = _getgenv.getscriptclosure
